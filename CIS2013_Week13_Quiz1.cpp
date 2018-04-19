1de0364c30a9fd7cf0c1a295bc51dd4547d94bc3
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-04-18 19:00:07 -0500

    v1.1

--- CIS2013_Week13_Quiz1.cpp
+++ CIS2013_Week13_Quiz1.cpp
@@ -5,22 +5,40 @@ using namespace std;
 class BankAccount{
 	public:
 	string name;
-	int acct_number;
-	long phone;
+	string acct_number;
+	string phone;
 	double balance;
-	double rate;
-	double deposit(double, double);
-	double withdraw(double, double);
+	double rate = 5.50;
+	double deposit();
+	double withdraw();
+	void print();
 	
 	BankAccount(){
-		string set_name();
-		long set_phone();
-		int set_account();
+		set_name();
+		set_phone();
+		set_account();
+		set_balance();
+		print();
 	}
 	string set_name(){
 		cout << "account name: ";
 		cin >> name;
 		return name;
+	}
+	string set_phone(){
+		cout << "Account phone number: ";
+		cin >> phone;
+		return phone;
+	}
+	string set_account(){
+		cout << "Account number: ";
+		cin >> acct_number;
+		return acct_number;
+	}
+	double set_balance(){
+		cout << "Starting balance: ";
+		cin >> balance;
+		return balance;
 	}
 	
 };
@@ -38,10 +56,14 @@ void menu();
 void choice();
 void open_account();
 
+Checking mine;
 
 int main(){
 	menu();
-	
+	while(true){
+		choice();
+	}
+
 	
 	return 0;
 }
@@ -54,15 +76,70 @@ void menu(){
 }
 void choice(){
 	char pick;
-	cout << "What would you like to do?: ";
-	cin >> pick;
-	if (pick == 'o'){}
-	
+	bool invalid = true;
+	while (invalid){
+		cout << "What would you like to do?: ";
+		cin >> pick;
+		if (pick == 'w'){
+			mine.withdraw();
+			invalid = false;
+		}
+		else if (pick=='d'){
+			mine.deposit();
+			invalid = false;
+		}
+		else if (pick=='t'){
+			mine.print();
+			invalid = false;
+		}	
+		else if (pick=='o'){
+			open_account();
+			invalid = false;
+		}
+		else {cout << "invalid entry - choose another.";
+		}
+	}
 }
 
-// void open_account
+void open_account(){
+	char a;
+	cout << "What type of account do you want to open? \n"
+		 << "Savings (s) or Checking (c): ";
+	cin >> a;
+	if (a=='c'){
+		Checking b;
+	}
+	else if (a=='s'){
+		Savings a;
+	}
+}
 
+double BankAccount::withdraw(){
+	double w;
+	cout << "How much would you like to withdrawal?: ";
+	cin >> w;
+	if (w>balance){
+		cout << "You have overdrawn.";
+	}
+	else {balance-=w;}
+	print();
+}
 
+double BankAccount::deposit(){
+	double d;
+	cout << "How much would you like to deposit?: ";
+	cin >> d;
+	balance+=d;
+	print();
+}
+
+void BankAccount::print(){
+	cout << "Name: " << name << endl
+		 << "Account number: " << acct_number << endl
+		 << "Phone number: " << phone << endl
+		 << "Balance: $" << balance << endl
+		 << "Interest Rate:" << rate << endl << endl; 
+}
 
 
 
